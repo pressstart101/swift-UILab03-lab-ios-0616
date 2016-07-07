873e4ffe90625a083175780660fb178e7e51eebc
pressstart101 <walterscott404@yandex.com>
2016-07-07 18:53:24 -0400

    ♣️

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     // TODO: Create IB outlets
+    @IBOutlet weak var lopeLeftLabel: UILabel!
+    @IBOutlet weak var bottomRightLabel: UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
 
+    @IBOutlet weak var four: UIButton!
+    @IBOutlet weak var three: UIButton!
+    @IBOutlet weak var eight: UIButton!
+    @IBOutlet weak var ten: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func cardTapped(sender: UIButton){
+        
+        if four.touchInside{
+            lopeLeftLabel.text = "♣️"
+            bottomRightLabel.text = "♣️"
+            middleLabel.text = "4"
+        }else if three.touchInside{
+            lopeLeftLabel.text = "♠️"
+            bottomRightLabel.text = "♠️"
+            middleLabel.text = "3"
+        }else if eight.touchInside{
+            lopeLeftLabel.text = "♦️"
+            bottomRightLabel.text = "♦️"
+            middleLabel.text = "8"
+        }else if ten.touchInside{
+            lopeLeftLabel.text = "♥️"
+            bottomRightLabel.text = "♥️"
+            middleLabel.text = "10"
+        }
+    //♣️4, ♠️3, ♦️8, ♥️10.
+    }
+    
     // TODO: IB actions and code to update UI
 }
